fe0f41a6531b7367dcd1ef28f42851d1d89e8d19
Alex-Zielinski <alex.zielinski.bc@gmail.com>
2017-04-09 08:39:33 -0700

    initialize REST

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -1,11 +1,19 @@
 <?php
 
-class Menu extends MY_Model {
+/**
+ * Modified to use REST client to get port data from our server.
+ */
+define('REST_SERVER', 'http://backend.local');  // the REST server host
+define('REST_PORT', $_SERVER['SERVER_PORT']);   // the port you are running the server on
+
+class Menu extends CI_Model {
 
 	// constructor
 	function __construct()
 	{
 		parent::__construct();
+		//*** Explicitly load the REST libraries. 
+		$this->load->library(['curl', 'format', 'rest']);
 	}
 
 	function rules() {
